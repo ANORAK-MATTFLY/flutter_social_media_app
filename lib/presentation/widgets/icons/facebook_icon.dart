ee2b067435a3fe2db287ded4f2ce8d03d5ef09b4
Ben Matanda <benmatanda354@gmail.com>
2021-08-17 13:53:21 +0100

    refactor: remove unncessary methods

--- lib/presentation/widgets/icons/facebook_icon.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-
-Widget facebookIcon =
-    Icon(Icons.facebook_outlined, size: 30.0, color: Colors.blue);
--- lib/presentation/widgets/icons/google_icon.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-Widget googleIcon = Container(
-  height: 45.0,
-  child: ClipRRect(
-    child: Image.asset("assets/images/google_icon.png"),
-  ),
-);
